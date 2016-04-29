91ad8b67158cd27106baa36093cc608933f8bbba
fske <fusion.ker@live.com>
2016-04-29 23:59:29 +0800

    d

--- models/analysis.js
+++ models/analysis.js
@@ -10,14 +10,17 @@ Analysis.get = function(callback) {
   pg.connect();
   var query = pg.query("SELECT gold_price FROM price_precious_metal ORDER BY bid_time DESC LIMIT 200;");
   var result = [];
-  var f = new Future;
-  query.on.future();
+  var f = new Future();
   query.on('row', function(row) {
+    console.info("row:" + row);
     result.push(row.gold_price);
-  }).wait();
-  query.on('end', client.end.bind(client));
-  console.log(result);
-  callback("", result);
+  });
+  f.return(true);
+  if (f.wait()) {
+    query.on('end', client.end.bind(client));
+    console.info("result:" + result);
+    callback("", result);
+  }
 };
 
 module.exports = Analysis;
